3f4235896a51466a60a056016633261e1c1d4e46
JRebadulla123 <jellianrebadulla@gmail.com>
2025-01-18 22:32:05 +0800

    Flex Box Direction

--- Components/AndroidPlatform/PlatformSpecificApp.android.js
+++ Components/AndroidPlatform/PlatformSpecificApp.android.js
@@ -4,11 +4,13 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 export default function PlatformSpecificApp() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your goal!" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your goal!" />
         <Button title="Add new goal" onPress={() => {}} />
       </View>
-      <Text>List of goals...</Text>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
@@ -16,5 +18,16 @@ export default function PlatformSpecificApp() {
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
   },
 });
