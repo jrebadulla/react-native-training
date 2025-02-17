57c0f166ec97a48d5deef7108d7f53e9c9b952d3
JRebadulla123 <jellianrebadulla@gmail.com>
2025-02-17 12:24:25 +0800

    Fix loop animation

--- Components/AndroidPlatform/PlatformSpecificApp.android.js
+++ Components/AndroidPlatform/PlatformSpecificApp.android.js
@@ -29,20 +29,25 @@ export default function PlatformSpecificApp() {
   const spinValue = useRef(new Animated.Value(0)).current;
   const { width, height } = useWindowDimensions();
   const isLandscape = width > height; 
+  const loopAnimation = useRef(null);
+
 
   useEffect(() => {
     if (loading) {
-      Animated.loop(
+      loopAnimation.current = Animated.loop(
         Animated.timing(spinValue, {
           toValue: 1,
           duration: 1000,
           useNativeDriver: true,
         })
-      ).start();
+      );
+      loopAnimation.current.start();
     } else {
-      spinValue.stopAnimation();
+      loopAnimation.current?.stop();
+      spinValue.setValue(0); // Reset animation
     }
   }, [loading]);
+  
 
   const spin = spinValue.interpolate({
     inputRange: [0, 1],
@@ -155,18 +160,21 @@ const debouncedSearch = useCallback(
   }, [filteredBooks, books, searchQuery]);
 
   const startLoadingAnimation = () => {
-    Animated.loop(
+    loopAnimation.current = Animated.loop(
       Animated.timing(spinValue, {
         toValue: 1,
         duration: 1000,
         useNativeDriver: true,
       })
-    ).start();
+    );
+    loopAnimation.current.start();
   };
 
   const stopLoadingAnimation = () => {
-    spinValue.stopAnimation();
+    loopAnimation.current?.stop();
+    spinValue.setValue(0);
   };
+  
 
   return (
     <LinearGradient colors={["#6D2323", "#FEF9E1"]} style={styles.appContainer}>
